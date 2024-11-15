c6581b800b8db0d08d402813d1f8a3ffaa96781e
PCyganiuk <pcyganiuk@gmail.com>
2024-11-15 22:14:10 +0100

    fixed directory issue

--- vite.config.ts
+++ vite.config.ts
@@ -4,4 +4,14 @@ import react from '@vitejs/plugin-react-swc'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  build: {
+    outDir: 'dist', // Ensures the build folder is named 'dist'
+    rollupOptions: {
+      output: {
+        manualChunks: {
+          vendor: ['react', 'react-dom'], // Split vendor dependencies for caching
+        },
+      },
+    },
+  },
 })
